4a37cfec9cc70a88d774f6b37f7db0df6567f8da
hahv3 <hahv3@vng.com.vn>
2021-07-16 15:31:30 +0700

    section 13: translator

---
+++ Section13/translator.py
@@ -0,0 +1,9 @@
+from translate import Translator
+
+translator = Translator(to_lang='ja')
+with open('./input.txt') as input_file:
+    text = input_file.read()
+    translation = translator.translate(text)
+
+with open('./output.txt', 'w') as output_file:
+    output_file.write(translation)
