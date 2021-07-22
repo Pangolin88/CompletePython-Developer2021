93ab12caa101e9748f7c158e7b1a81d7b6dfc7f8
hahv3 <hahv3@vng.com.vn>
2021-07-22 23:47:21 +0700

    break-the-ice-with-python: 70 --> 74

---
+++ break-the-ice-with-python/question71.py
@@ -0,0 +1,10 @@
+"""
+Please write a program to output a random number, which is divisible by 5 and 7,
+between 10 and 150 inclusive using random module and list comprehension.
+"""
+
+
+import random
+
+options = [i for i in range(50, 150) if i % 35 == 0]
+print(random.choice(options))
