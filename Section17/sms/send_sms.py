5d6a0809656304819ff2b27ad68abfa12cb3483a
hahv3 <hahv3@vng.com.vn>
2021-07-24 14:17:06 +0700

    Section17: send sms

---
+++ Section17/sms/send_sms.py
@@ -0,0 +1,16 @@
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = ''
+auth_token = ''
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+14434007805',
+                     to='+84766901516'
+                 )
