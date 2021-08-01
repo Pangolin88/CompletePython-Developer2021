7c0ed916f60786485b949d29ac1099fa56830779
hahv3 <hahv3@vng.com.vn>
2021-08-01 10:20:33 +0700

    section 19

--- Section19/portfolio/server.py
+++ Section19/portfolio/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,4 +11,28 @@ def my_home():
 @app.route('/<string:page_name>')
 def index(page_name='/index.html'):
     return render_template(page_name)
+
+def write_to_csv(data):
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+    with open('database.csv', mode='a', newline='') as database:
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    print('request.method: ', request.method)
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'Did not save to database!'
+
+    else:
+        return 'Something went wrong!'
+
+
 
